839a439e115e071ffffa494b29312a683fd6ec70
Kenny Colsh <Kenny.Colsh@alkermes.com>
2019-02-13 13:51:18 +0000

    Got the input from user working

--- Solution-1.py
+++ Solution-1.py
@@ -1,1 +1,10 @@
 # Solution to problem 1 
+age = input("What is your age? ")
+print(age)
+#print "Your age is: ", age
+type(age)
+
+#>>output
+#What is your age? 100
+#Your age is:  100
+#type 'int'>
