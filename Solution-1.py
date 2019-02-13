0ea8720f099abb0756fd5b0367178d0dc1bcd73a
Kenny Colsh <Kenny.Colsh@alkermes.com>
2019-02-13 17:42:32 +0000

    Sorted the added of data

--- Solution-1.py
+++ Solution-1.py
@@ -3,14 +3,18 @@
 user_input  = input("Please enter a positive integer:")
 try:
    val = int(user_input)
+   ans = 0
    print("Yes input string is an Integer.")
    if val > 0:
     print("Good Work its greater than 1!")
+    i=1
+    while i<= val:
+        ans =ans+i
+        i=i+1
+    print(ans)
    else:
-  
-    print("Try and Pick a Number greater and 1")
+      print("Try and Pick a Number greater and 1")
 
-       
         #print("Input number value is: ", val)
 except ValueError:
    print("That's not an int!")
