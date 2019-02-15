30ac96843300ab44b1c16760098b386cc6b70ffe
Kenny Colsh <Kenny.Colsh@alkermes.com>
2019-02-15 20:37:28 +0000

    Complete Solution 2 -15-02-19

--- Solution-2.py
+++ Solution-2.py
@@ -1,3 +1,22 @@
 #Problem 2 15-2-19 Kenny Colsh
 #Get the day of the week
 #check it the day "Startswith" a "T"
+#https://stackoverflow.com/questions/9847213/how-do-i-get-the-day-of-week-given-a-date-in-python
+#https://www.w3schools.com/python/showpython.asp?filename=demo_ref_string_startswith
+#https://stackoverflow.com/questions/17953940/yes-or-no-output-python
+from datetime import date
+import calendar
+my_date = date.today()
+print("Today is : " + calendar.day_name[my_date.weekday()])  
+
+dayofweek =calendar.day_name[my_date.weekday()]
+
+
+x = dayofweek.startswith("T")
+if x :
+  print("Yes - today begins with a T.")
+else:
+  print ("No - today does not begin with a T.")
+  
+
+
