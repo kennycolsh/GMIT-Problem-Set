911f849ea6d8f84784c88d58e602afd9d7c4dc1e
Kenny Colsh <Kenny.Colsh@alkermes.com>
2019-02-15 13:58:00 +0000

    Final Q1 problem set

--- Solution-1.py
+++ Solution-1.py
@@ -1,20 +1,23 @@
 # Solution to problem 1 
 #https://www.pythonforbeginners.com/basics/getting-user-input-from-the-keyboard
-user_input  = input("Please enter a positive integer:")
-try:
-   val = int(user_input)
-   ans = 0
-   #print("Yes input string is an Integer.")
-   if val > 0:
-    print("Good Work its an Interger and greater than 1!")
-    i=1
-    while i<= val:
-        ans =ans+i
-        i=i+1
-    print(ans)
-   else:
-      print("Try and Pick a Number greater and 0")
 
-        #print("Input number value is: ", val)
+   
+try:
+       user_input  = input("Please enter a positive integer:")
+       val = int(user_input)
+       ans = 0
+         #print("Yes input string is an Integer.")
+       if val > 0:
+           print("Good Work its an Interger and greater than 1!")
+           i=1
+           while i<= val:
+             ans =ans+i
+             i=i+1
+           print(ans)             
+       else:
+            print("Try and Pick a Number greater and 0")
+      #print("Input number value is: ", val)
 except ValueError:
-   print("No.. input string is not an Integer. It's a string")
+      print("No.. input string is not an Integer. It's a string")
+  
+  
