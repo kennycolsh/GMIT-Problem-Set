f4ac72f740f77771de86e1b245af1d8e1c2a1ed8
Kenny Colsh <Kenny.Colsh@alkermes.com>
2019-03-05 21:16:37 +0000

    Final Solution 4 05-03-19

--- collatz.py
+++ collatz.py
@@ -1,4 +1,5 @@
 ##Problem Set Solution 4
+#create function collatz and pass it a variable number
 def collatz(number):
 	ans=""
 	while number > 1:
@@ -9,7 +10,8 @@ def collatz(number):
 		else:
 			number = (number*3)+1
 			ans = ans + ', ' + str(number)
-           
+#return the result of the function where the string var "ans"
+# concats a number to a string           
 	return ans
 number = input("Enter a number: ")
 
