bf4742d2789d5c5f34f9e4339f05ef6a6e44e114
Kenny Colsh <Kenny.Colsh@alkermes.com>
2019-03-05 21:14:06 +0000

    ConCat int to str Solution 4 05-03-19

--- collatz.py
+++ collatz.py
@@ -4,12 +4,10 @@ def collatz(number):
 	while number > 1:
 		if number % 2 == 0:
 			number = number//2
-			print(number)
 			ans = ans + ', ' + str(number)
             
 		else:
 			number = (number*3)+1
-			print(number)
 			ans = ans + ', ' + str(number)
            
 	return ans
