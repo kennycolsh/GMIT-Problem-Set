0956255b4614d068990a0c15b339560836c3e7b5
Kenny Colsh <Kenny.Colsh@alkermes.com>
2019-03-06 17:39:02 +0000

    Working Solution need to update readMe Solution 5

--- Solution-5-Prime.py
+++ Solution-5-Prime.py
@@ -14,23 +14,21 @@ def is_prime_number(x):
 while  quit < 1:
 
    try:
+       #get user integer
          user_input  = input("Please enter a positive integer:")
          val = int(user_input)
-         ans = 0
-            #print("Yes input string is an Integer.")
+         ans = ""
+            
          if val > 0:
-            print("Good Work its an Interger and greater than 1!")
-            i=1
-            while i<= val:
-               ans =ans+i
-               i=i+1
-            print(ans)             
+             #Call the function
+               ans = is_prime_number(val)
+               print(ans)            
          else:
                print("Try and Pick a Number greater and 0")
 
          quit1  = input("Enter an interger less than 1 to quit")
          quit = int(quit1)
          print(quit)
-         #print("Input number value is: ", val)
+         
    except ValueError:
          print("No.. input string is not an Integer. It's a string")
