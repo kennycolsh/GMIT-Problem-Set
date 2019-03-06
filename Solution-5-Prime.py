89ba700758f81494600e35b9b24e8b4b3e310655
Kenny Colsh <Kenny.Colsh@alkermes.com>
2019-03-06 17:31:24 +0000

    Copied Solution 1 to as a starting point Solution 5

---
+++ Solution-5-Prime.py
@@ -0,0 +1,28 @@
+#Solution for problem 5
+#Ref
+#https://linuxconfig.org/function-to-check-for-a-prime-number-with-python
+#this is the starting point--Solution 1
+quit =0
+while  quit < 1:
+
+   try:
+         user_input  = input("Please enter a positive integer:")
+         val = int(user_input)
+         ans = 0
+            #print("Yes input string is an Integer.")
+         if val > 0:
+            print("Good Work its an Interger and greater than 1!")
+            i=1
+            while i<= val:
+               ans =ans+i
+               i=i+1
+            print(ans)             
+         else:
+               print("Try and Pick a Number greater and 0")
+
+         quit1  = input("Enter an interger less than 1 to quit")
+         quit = int(quit1)
+         print(quit)
+         #print("Input number value is: ", val)
+   except ValueError:
+         print("No.. input string is not an Integer. It's a string")
