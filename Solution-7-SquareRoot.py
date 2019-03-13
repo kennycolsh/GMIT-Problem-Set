374ff4d2689e45b8594a89f6b4e399f4eb7ed67e
Kenny Colsh <Kenny.Colsh@alkermes.com>
2019-03-13 10:14:46 +0000

    Adidng fuction to check for int and float Solution 7

--- Solution-7-SquareRoot.py
+++ Solution-7-SquareRoot.py
@@ -1,1 +1,13 @@
 #Solution for problem 7
+
+user_input  = input("Please enter a positive Float:")
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+print(is_number(user_input))
