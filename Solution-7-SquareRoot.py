718e4366f6cfe259decc4ad65672b6aea42f03ec
Kenny Colsh <Kenny.Colsh@alkermes.com>
2019-03-13 11:01:36 +0000

    Final including output back to string Solution 7

--- Solution-7-SquareRoot.py
+++ Solution-7-SquareRoot.py
@@ -1,6 +1,7 @@
 #Solution for problem 7
 #https://stackoverflow.com/questions/34425583/how-to-check-if-string-is-int-or-float-in-python-2-7?answertab=votes#tab-top
 #import the math lib
+#https://stackoverflow.com/posts/3400987/edit for rounding
 
 import math
 user_input  = (input("Please enter a positive Float:"))
@@ -20,5 +21,7 @@ def is_number(s):
 if (is_number(user_input)) == True:
         print("Its a float.")
         ans = math.sqrt(float(user_input))
+        #round ans
         rans =round(ans, 1)
-        print(rans)
+        #had to convert back the flooat to a type of string for display.
+        print("The square root of " + user_input + " is approx. "+ str(rans))
