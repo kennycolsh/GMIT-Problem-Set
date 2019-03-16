5b5f4a20b066a3504a6526b95c169899cf5af8b0
Kenny Colsh <Kenny.Colsh@alkermes.com>
2019-03-16 06:32:18 +0000

    Setting up Solution 9

--- Solution-9-Files.py
+++ Solution-9-Files.py
@@ -1,2 +1,7 @@
 #Solution for problem 8
+#https://docs.python.org/3/tutorial/inputoutput.html#saving-structured-data-with-json
+#7.2. Reading and Writing Files
+#https://web.microsoftstream.com/video/72484dfc-1b50-4223-8039-bd6a69cab573
 
+
+user_input  = (input("Enter the name of the file : "))
