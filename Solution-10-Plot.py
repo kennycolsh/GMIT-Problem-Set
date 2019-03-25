2949e77a025099f2d8b781e7422ee979f73d0f6c
Kenny Colsh <Kenny.Colsh@alkermes.com>
2019-03-25 15:19:09 +0000

    Trying with old Faith full Solution 10

--- Solution-10-Plot.py
+++ Solution-10-Plot.py
@@ -1,12 +1,19 @@
 #Solution for problem 10
-import matplotlib.pyplot as plt
-plt.plot([1,2,3,4])
-plt.ylabel('some numbers')
-plt.show()
+#import matplotlib.pyplot as plt
+#plt.plot([1,2,3,4])
+#plt.ylabel('some numbers')
+#plt.show()
 
-import numpy
+import numpy as np
 import matplotlib.pyplot as pl
 
-pl.hist(firstcol)
-numpy.min(firstcol)
-numpy.max(firstcol)
+data =np.genfromtxt('Faith.csv',delimiter=',',skip_header=True)
+firstcol = data[:,0]
+meanfirstcol = numpy.mean(data[:,0])
+
+
+print("Average " ,meanfirstcol)
+
+#pl.hist(firstcol)
+#numpy.min(firstcol)
+#numpy.max(firstcol)
