ac347c73005835554be29b9db7f8546257da267b
Kenny Colsh <Kenny.Colsh@alkermes.com>
2019-03-25 12:01:19 +0000

    Starting Solution 10

---
+++ Solution-10-Plot.py
@@ -0,0 +1,12 @@
+#Solution for problem 10
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.show()
+
+import numpy
+import matplotlib.pyplot as pl
+
+pl.hist(firstcol)
+numpy.min(firstcol)
+numpy.max(firstcol)
