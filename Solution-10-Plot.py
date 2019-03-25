5d60e9bdf502b079a4e3aabfcde2b9f73e23946e
Kenny Colsh <Kenny.Colsh@alkermes.com>
2019-03-25 16:24:32 +0000

    Final effort with comments Solution 10

--- Solution-10-Plot.py
+++ Solution-10-Plot.py
@@ -1,19 +1,37 @@
 #Solution for problem 10
-#import matplotlib.pyplot as plt
-#plt.plot([1,2,3,4])
-#plt.ylabel('some numbers')
-#plt.show()
+#https://pythonspot.com/matplotlib-legend/
+ 
 
 import numpy as np
 import matplotlib.pyplot as pl
 
-data =np.genfromtxt('Faith.csv',delimiter=',',skip_header=True)
-firstcol = data[:,0]
-meanfirstcol = np.mean(data[:,0])
-pl.hist(firstcol)
-
-print("Average " ,meanfirstcol)
+#data =np.genfromtxt('Faith.csv',delimiter=',',skip_header=True)
+#firstcol = data[:,0]
+#meanfirstcol = np.mean(data[:,0])
+#pl.hist(firstcol)
+#pl.show()
+#print("Average " ,meanfirstcol)
 
 #pl.hist(firstcol)
 #numpy.min(firstcol)
 #numpy.max(firstcol)
+
+#set up the array
+#start, stop, step
+x = np.arange(0, 4, .5)
+#create the variables
+y_1 = x
+y_2 = x**2
+y_3 = 2**x
+#plot the graph and create the label
+pl.plot(x, y_1, label='y=x')
+pl.plot(x, y_2, label='y=x^2')
+pl.plot(x, y_3, label='y=2^x')
+#give it grid lines
+pl.grid()
+#add legends to make it more readable
+pl.legend()
+#give the graph a title
+pl.title('Solution 10')
+#show the Plot
+pl.show()
